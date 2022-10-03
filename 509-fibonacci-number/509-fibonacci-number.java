f0eed1a61e126185cdba06839e6a22ede3a07db3
Akash Singh Kushwah <77046910+headshot0317@users.noreply.github.com>
2022-10-03 16:07:46 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (86.11%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int fib(int n) {
+        int dp[]=new int[33];
+        dp[0]=0;dp[1]=1;
+        for(int i=2;i<=30;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+}
