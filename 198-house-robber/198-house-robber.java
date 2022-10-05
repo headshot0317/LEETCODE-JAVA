3f35468b96fef154f36e46bdfc94b2cae34af305
Akash Singh Kushwah <77046910+headshot0317@users.noreply.github.com>
2022-10-05 12:48:27 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (29.28%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -3,14 +3,18 @@ class Solution {
         if(nums.length == 1) {
             return nums[0];
         }
-
-        int before_last_rob = 0;
-        int prev_rob = nums[0]; 
-        for (int i = 1; i < nums.length; i++) {   
-            int rob = Math.max(nums[i] + before_last_rob,prev_rob);
-            before_last_rob = prev_rob;
-            prev_rob = rob;
+    int dp[]=new int[nums.length];
+        if(nums.length==1){
+            return nums[0];
+        }else if(nums.length==2){
+            return Math.max(nums[0],nums[1]);
+        }else{  
+         dp[0]=nums[0];  
+            dp[1]=Math.max(nums[0],nums[1]);
+            for(int i=2;i<nums.length;i++){
+               dp[i]=Math.max(dp[i-1],nums[i]+dp[i-2]); 
+            }
         }
-        return prev_rob;
+        return dp[nums.length-1];
     }
 }
